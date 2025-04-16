67c674b96d54d8965ac0d5339641eac359a8ebcb
awais-iftikhar79 <awaischoudhary0709@gmail.com>
2025-04-16 19:51:47 +0500

    question of list

---
+++ 2nd_Semester/OOP_in_c++/Practice/STL /Containers/List/insert_front_back.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+template <typename T>
+void display(list<T> l)
+{
+    cout << "Elements in list are : ";
+    for (auto it = l.begin(); it != l.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    list<int> l;
+    int val;
+    size_t n;
+    cout << "Enter size of list " << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element at index " << i << endl;
+        cin >> val;
+        l.push_back(val);
+    }
+
+    display(l);
+
+    cout << "Enter value you want to add in start" << endl;
+    cin >> val;
+    l.push_front(val);
+
+    cout << "Enter value you want to add in end" << endl;
+    cin >> val;
+    l.push_back(val);
+
+    display(l);
+
+    return 0;
+}
