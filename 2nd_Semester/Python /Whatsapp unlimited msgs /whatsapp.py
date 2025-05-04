051d67441f2b2b448b61ca27b039d66717623c1f
awais-iftikhar79 <awaischoudhary0709@gmail.com>
2025-05-04 09:53:30 +0500

    trying to install pyautogui python libaray

---
+++ 2nd_Semester/Python /Whatsapp unlimited msgs /whatsapp.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+time.sleep(4)
+count =0
+while count <=50:
+    pyautogui.typewrite("Sooooooooooooooooooooooooooooooooooooooooooryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+    pyautogui.press("enter")
+    time.sleep(2)
+    count = count +1
