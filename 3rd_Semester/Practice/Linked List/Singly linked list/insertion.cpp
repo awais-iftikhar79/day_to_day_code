d427dfac02768a9f29cd77b607843f4699da18fb
awais-iftikhar79 <awaischoudhary0709@gmail.com>
2025-09-24 01:42:00 +0500

    general function for inserting node by pasing index that works for all cases

---
+++ 3rd_Semester/Practice/Linked List/Singly linked list/insertion.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class Linked_List
+{
+private:
+    Node *first;
+
+public:
+    Linked_List()
+    {
+        first = nullptr;
+    }
+
+    void insertion(int index, int value)
+    {
+        if (index < 0 || index > count_nodes())
+        {
+            cout << "index is invalid !" << endl;
+            return;
+        }
+        Node *new_node = new Node, *p;
+
+        if (index == 0)
+        {
+            new_node->data = value;
+            new_node->next = first;
+            first = new_node;
+        }
+        else
+        {
+            p = first;
+
+            for (int i = 0; i < index - 1; i++)
+            {
+                p = p->next;
+            }
+
+            new_node->data = value;
+            new_node->next = p->next;
+            p->next = new_node;
+        }
+    }
+
+    int count_nodes() // by using loop
+    {
+        Node *p = first;
+        int lenght = 0;
+
+        while (p)
+        {
+            lenght++;
+            p = p->next;
+        }
+        return lenght;
+    }
+    void display()
+    {
+        Node *p = first;
+        while (p != nullptr)
+        {
+            cout << p->data << " -> ";
+            p = p->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main()
+{
+    Linked_List list;
+    list.insertion(0, 5);
+    list.insertion(1, 10);
+    list.insertion(2, 15);
+    list.insertion(3, 20);
+    list.insertion(0, 0);
+    list.display();
+
+    return 0;
+}
