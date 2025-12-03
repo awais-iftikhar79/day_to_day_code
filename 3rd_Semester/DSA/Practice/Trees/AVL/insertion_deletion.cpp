63b6be283dc9f6972599f34ad048c37b72c9fcd0
awais-iftikhar79 <awaischoudhary0709@gmail.com>
2025-12-03 19:23:07 +0500

    adding deleting function AVL tree

--- 3rd_Semester/DSA/Practice/Trees/AVL/insertion_deletion.cpp
+++ 3rd_Semester/DSA/Practice/Trees/AVL/insertion_deletion.cpp
@@ -24,11 +24,17 @@ public:
         root = insertion(root, key);
     }
     Node *insertion(Node *p, int key);
+    Node *myDelete(Node *p, int key);
+    void myDelete(int key) { root = myDelete(root, key); }
 
     void inorder()
     {
         inorder(root);
     }
+
+    Node *in_pre(Node *p);
+    Node *in_succ(Node *p);
+
     void inorder(Node *p);
     int height(Node *p);
     int balancing_fact(Node *p);
@@ -47,6 +53,24 @@ int AVL::height(Node *p)
     hr = (p && p->r_child) ? p->height : 0;
 
     return (hl > hr) ? hl + 1 : hr + 1;
+}
+
+Node *AVL::in_pre(Node *p)
+{
+    while (p && p->r_child != nullptr)
+    {
+        p = p->r_child;
+    }
+    return p;
+}
+
+Node *AVL::in_succ(Node *p)
+{
+    while (p && p->l_child != nullptr)
+    {
+        p = p->l_child;
+    }
+    return p;
 }
 
 int AVL::balancing_fact(Node *p)
@@ -182,6 +206,89 @@ Node *AVL::insertion(Node *p, int key)
     return p;
 }
 
+Node *AVL::myDelete(Node *p, int key)
+{
+    Node *q;
+    int balance;
+
+    if (p == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (key > p->data)
+    {
+        p->r_child = myDelete(p->r_child, key);
+    }
+
+    else if (key < p->data)
+    {
+        p->l_child = myDelete(p->l_child, key);
+    }
+
+    else
+    {
+        if (p->l_child == nullptr && p->r_child == nullptr)
+        {
+            delete p;
+            return nullptr;
+        }
+
+        else if (p->l_child != nullptr && p->r_child == nullptr)
+        {
+            Node *temp = p->l_child;
+            delete p;
+            return temp;
+        }
+
+        else if (p->l_child == nullptr && p->r_child != nullptr)
+        {
+            Node *temp = p->r_child;
+            delete p;
+            return temp;
+        }
+
+        else
+        {
+            if (height(p->l_child) > height(p->r_child))
+            {
+                q = in_pre(p->l_child);
+                p->data = q->data;
+                p->l_child = myDelete(p->l_child, q->data);
+            }
+
+            else
+            {
+                q = in_succ(p->r_child);
+                p->data = q->data;
+                p->r_child = myDelete(p->r_child, q->data);
+            }
+        }
+    }
+
+    p->height = height(p);
+    balance = balancing_fact(p);
+
+    if (balancing_fact(p) == 2 && balancing_fact(p->l_child) == 1)
+    {
+        return LL_rotaion(p);
+    }
+    else if (balancing_fact(p) == -2 && balancing_fact(p->r_child) == -1)
+    {
+        return RR_rotaion(p);
+    }
+    else if (balancing_fact(p) == 2 && balancing_fact(p->l_child) == -1)
+    {
+        return LR_rotaion(p);
+    }
+    else if (balancing_fact(p) == -2 && balancing_fact(p->r_child) == 1)
+    {
+        return RL_rotaion(p);
+    }
+
+    return p;
+}
+
 void AVL::inorder(Node *p)
 {
     if (p)
@@ -205,6 +312,10 @@ int main()
     avl.insertion(5);
     avl.insertion(15); // output 5 10 15 20 25 30 35 40 50  (all rorations are used in it)
 
+    avl.inorder();
+    cout << endl;
+    avl.myDelete(25);
+    cout << endl;
     avl.inorder();
 
     return 0;
